47eae9f06307e53e1ab37e60e2692c5ae71760ae
Arpit Bhushan Sharma <42774305+arpit1920@users.noreply.github.com>
2021-04-04 12:06:54 +0530

    Add files via upload

---
+++ day 4/START01.cpp
@@ -0,0 +1,21 @@
+/*
+Problem Statement
+Write a program that accepts a number, n, and outputs the same.
+
+Input
+The only line contains a single integer.
+
+Output
+Output the answer in a single line.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	cout<<n<<endl;
+	return 0;
+}
